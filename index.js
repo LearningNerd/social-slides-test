9799cad52291b11b658039d4069c0a82b3952ead
LearningNerd <learningnerd@gmail.com>
2016-07-04 14:27:14 -0700

    changed to use variable port to work on Heroku

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ var express = require("express");
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var port = process.env.PORT || 8000;
 
 app.use(express.static('public'));
 
-http.listen(8000, function(){
-  console.log('listening on *:8000');
+http.listen(port, function(){
+  console.log('listening on ' + port);
 });
 
 var numSockets = 0;
