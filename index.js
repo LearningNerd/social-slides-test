1d767ab8f6be5d01c749d6642856e1afecfa211c
LearningNerd <learningnerd@gmail.com>
2016-08-22 12:42:21 -0700

    some slide updates

--- index.js
+++ index.js
@@ -13,14 +13,14 @@ app.use(cookieParser('secretive secret cookie keyyyy'));
 
 // "Secret" login route to grant slidemaster privileges with a cookie
 app.get('/login', function (req, res) {
-    res.cookie('yummycookie', 'this is a test', {maxAge: 10800000, httpOnly: true, signed: true});
-  	res.redirect('/');
+	res.cookie('yummycookie', 'this is a test', {maxAge: 10800000, httpOnly: true, signed: true});
+	res.redirect('/');
 });
 
 // Logout to revoke slidemaster privileges
 app.get('/logout', function(req,res){
-  res.clearCookie('yummycookie');
-  res.send('cleared');
+	res.clearCookie('yummycookie');
+	res.send('cleared');
 });
 
 // Serve static files
@@ -28,53 +28,53 @@ app.use(express.static('public'));
 
 // SocketIO stuff:
 io.on('connection', function(socket){
-  console.log('\nA user connected!');
+	console.log('\nA user connected!');
 
-  // Parse and validate cookie
-  var parsedCookies = cookie.parse(socket.handshake.headers.cookie)
-  var signedcookie = cookieParser.signedCookie (parsedCookies['yummycookie'], 'secretive secret cookie keyyyy');
-      console.log('signed cookie value: ', signedcookie);
+	// Parse and validate cookie
+	var parsedCookies = cookie.parse(socket.handshake.headers.cookie)
+	var signedcookie = cookieParser.signedCookie (parsedCookies['yummycookie'], 'secretive secret cookie keyyyy');
+	console.log('signed cookie value: ', signedcookie);
 
-  // Only grant slidemaster privileges if client sent valid cookie
-  if (parsedCookies['yummycookie'] !== signedcookie) {
+	// Only grant slidemaster privileges if client sent valid cookie
+	if (parsedCookies['yummycookie'] !== signedcookie) {
 
-    // signal to client to set "slidechanged" listeners
-    socket.emit('new master', socket.id);
-    console.log('Slide master connected and confirmed. id: ' + socket.id);
+		// signal to client to set "slidechanged" listeners
+		socket.emit('new master', socket.id);
+		console.log('Slide master connected and confirmed. id: ' + socket.id);
 
-    // Allow this client to broadcast to other clients
-    socket.on("slidechanged", function (msg) {
-      console.log('Slide master changed slide! Now broadcasting to users.')
-      socket.broadcast.emit("slidechanged", msg);
-    });
+		// Allow this client to broadcast to other clients
+		socket.on("slidechanged", function (msg) {
+			console.log('Slide master changed slide! Now broadcasting to users.')
+			socket.broadcast.emit("slidechanged", msg);
+		});
 
-    socket.on('disconnect', function(msg){
-        console.log('Slide master disconnected!');
-    });
+		socket.on('disconnect', function(msg){
+			console.log('Slide master disconnected!');
+		});
 
-  } // end of section for slidemaster privileges
+	} // end of section for slidemaster privileges
 
-  socket.on('disconnect', function(msg){
-      console.log('A user disconnected.');
-  });
-
-
-  // CHAT-DEMO
-  socket.on('chat', function(data){
-      console.log('CHAT: name: '+ data.name + ', message: ' + data.message);
-      io.emit('chat', data);
-  });
+	socket.on('disconnect', function(msg){
+		console.log('A user disconnected.');
+	});
 
 
-  // TEST1
-  socket.on('move it', function(data){
-      console.log(data);
-      io.emit('move it', data);
-  });
+	// CHAT-DEMO
+	socket.on('chat', function(data){
+		console.log('CHAT: name: '+ data.name + ', message: ' + data.message);
+		io.emit('chat', data);
+	});
+
+
+	// TEST1
+	socket.on('move it', function(data){
+		console.log(data);
+		io.emit('move it', data);
+	});
 
 }); // end of io.on('connection' ...
 
 // Server, listen up!
 http.listen(port, function(){
-  console.log('listening on ' + port);
+	console.log('listening on ' + port);
 });
